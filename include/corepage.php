e974809fb98a0f922bf0745cbcb566b9a31a39da
i_82 <i.82@qq.com>
2014-10-14 12:28:16 +0800

    LICENSE

--- include/corepage.php
+++ include/corepage.php
@@ -1,22 +1,20 @@
 <?php
 	/*
-		This file is part of WEIPDCRM.
+		This file is part of DuoLuo.
 	
-	    WEIPDCRM is free software: you can redistribute it and/or modify
+	    DuoLuo is free software: you can redistribute it and/or modify
 	    it under the terms of the GNU General Public License as published by
 	    the Free Software Foundation, either version 3 of the License, or
 	    (at your option) any later version.
 	
-	    WEIPDCRM is distributed in the hope that it will be useful,
+	    DuoLuo is distributed in the hope that it will be useful,
 	    but WITHOUT ANY WARRANTY; without even the implied warranty of
 	    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	    GNU General Public License for more details.
 	
 	    You should have received a copy of the GNU General Public License
-	    along with WEIPDCRM.  If not, see <http://www.gnu.org/licenses/>.
+	    along with DuoLuo.  If not, see <http://www.gnu.org/licenses/>.
 	*/
-	
-	/* DCRM List Footer Control */
 	
 	if (!defined("DCRM")) {
 		exit();
